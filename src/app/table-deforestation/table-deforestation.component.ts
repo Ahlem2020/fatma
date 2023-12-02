21431c51d949b363bd96c9cafa40832348917ba1
Ahlem2020 <ahlem.benfradj@esprit.tn>
2023-12-02 19:09:27 +0100

    first commit

---
+++ src/app/table-deforestation/table-deforestation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-deforestation',
+  templateUrl: './table-deforestation.component.html',
+  styleUrls: ['./table-deforestation.component.scss']
+})
+export class TableDeforestationComponent {
+
+}
---
+++ src/app/table-grassland/table-grassland.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-grassland',
+  templateUrl: './table-grassland.component.html',
+  styleUrls: ['./table-grassland.component.scss']
+})
+export class TableGrasslandComponent {
+
+}
